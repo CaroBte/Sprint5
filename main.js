f33e3f0a71248af256a746dc14ba79c0443235ad
CaroBte <karito0721@hotmail.com>
2022-12-10 14:52:47 -0500

    adding Date method

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-console.log("Hola Mundo");
+console.log("Hello world");
 
 const getData = () => {
     const btn = document.getElementById("btn")
     btn.addEventListener("click", (e) => {
         e.preventDefault();
-        const fecha = document.querySelector("#date").value;
-        console.log(fecha);
+        const date = document.querySelector("#date").value;
+        console.log(date);
     })
 }
 
@@ -13,3 +13,16 @@ const getData = () => {
     const div = document.getElementById("newDate");
     div.innerHTML = `La fecha seleccionada es: ${fecha}`
 } */
+
+const months = document.getElementById("months");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const actualDate = new Date();
+const countdownDate = new Date(2022 - 12 - 31)
+const remainTime = actualDate - countdownDate;
+
+console.log(`The actual date is: ${actualDate}`);
+console.log(`The countdown date is: ${countdownDate}`)
+console.log(`The remaining time is: ${remainTime}`)
