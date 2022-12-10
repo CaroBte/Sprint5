357306e56df58a355dc00dac04f40dcfea52f32e
CaroBte <karito0721@hotmail.com>
2022-12-10 17:15:55 -0500

    fixing bugs and setInterval

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 console.log("Hello world");
-console.log(new Date);
 
-const getData = () => {
-    const date = document.querySelector("#date").value;
-    const inputDate = new Date(date);
-    const actualDate = new Date();
-    updateCountdown(inputDate - actualDate)
-}
-
-const months = document.getElementById("months");
-const days = document.getElementById("days");
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
+const monthsDiv = document.getElementById("months");
+const daysDiv = document.getElementById("days");
+const hoursDiv = document.getElementById("hours");
+const minutesDiv = document.getElementById("minutes");
+const secondsDiv = document.getElementById("seconds");
 
 const updateCountdown = (remainTime) => {
     const months = Math.floor(remainTime / 1000 / 60 / 60 / 24 / 30)
-    console.log(`Months: ${months}`);
     const days = Math.floor(remainTime / 1000 / 60 / 60 / 24) % 30
-    console.log(`Days: ${days}`);
-    const hours = Math.floor(remainTime / 1000 / 60 / 60)
-    console.log(`Hours: ${hours}`);
+    const hours = Math.floor(remainTime / 1000 / 60 / 60) % 24
     const minutes = Math.floor(remainTime / 1000 / 60) % 60
-    console.log(`Minutes: ${minutes}`);
     const seconds = Math.floor(remainTime / 1000) % 60
-    console.log(`Seconds: ${seconds}`);
+
+    monthsDiv.innerHTML = months
+    daysDiv.innerHTML = days
+    hoursDiv.innerHTML = hours
+    minutesDiv.innerHTML = minutes
+    secondsDiv.innerHTML = seconds
+}
+
+const getDate = () => {
+    const date = document.querySelector("#date").value;
+    const inputDate = new Date(`${date}T00:00:00`);
+
+    setInterval(() => {
+        const actualDate = new Date();
+        updateCountdown(inputDate - actualDate)
+    }, 1000)
+
 }
